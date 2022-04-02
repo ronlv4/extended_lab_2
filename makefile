2ebafcaf53fae3ffd6707458f22f4d648ce92ece
Ron Levi <ronlv4@gmail.com>
2022-04-02 23:51:15 +0300

    up to t2c, not all works

--- makefile
+++ makefile
@@ -1,10 +1,20 @@
 # All Targets
-all: task0
+all: task0 t1a t1d t2
 
 libs: c-lib
 
 task0: count-words.c
 	gcc -m32 -g -Wall -o count count-words.c
+
+t1a: addresses.c
+	gcc -m32 -g -Wall -o t1a addresses.c
+
+t1d: t1d.c
+	gcc -m32 -g -Wall -o t1d t1d.c
+
+t2: t2.c
+	gcc -m32 -g -Wall -o t2 t2.c
+
 
 # task0.o: count-words.c
 # 	gcc -g -Wall -m32 -ansi -c -o task0.o countwords.c
@@ -14,4 +24,4 @@ task0: count-words.c
 
 #Clean the build directory
 clean:
-	rm -f *.o count
+	rm -f *.o count t1a t1b t2
--- t1d.c
+++ t1d.c
@@ -31,19 +31,28 @@ int main (int argc, char** argv){
     printf("- &foo1: %p\n" ,&foo2);
     printf("- &foo2 - &foo1: %ld\n" ,&foo2 - &foo1);
 
-    int iarray[3];
-    float farray[3];
-    double darray[3];
-    char carray[3];
+    int iarray[] = {1, 2, 3};
+    char carray[] = {'a', 'b', 'c'};
+    int* iarrayPtr;
+    char* carrayPtr;
+    iarrayPtr = &iarray;
+    carrayPtr = &carray;
     
-    printf("- iarray: %p\n" ,&iarray);
-    printf("- iarray + 1: %p\n" ,&iarray + 1);
-    printf("- farray: %p\n" ,&farray);
-    printf("- farray + 1: %p\n" ,&farray + 1);
-    printf("- darray: %p\n" ,&darray);
-    printf("- darray + 1: %p\n" ,&darray + 1);
-    printf("- carray: %p\n" ,&carray);
-    printf("- carray + 1: %p\n" ,&carray + 1);
+    printf("iarray:\n");
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+	    printf("%d\n", *(iarrayPtr + i));
+    }
+    
+    printf("carray:\n");
+    for (i = 0; i < 3; i++)
+    {
+	    printf("%c\n", *(carrayPtr + i));
+    }
+    
+    int* p;
+    printf("unintialized local pointer: %p\n", p); 
     return 0;
 }
 
